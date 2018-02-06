7dab46a3b06640a1782320797111f5096b645dd5
NaziaAnjumSharupa <nasxotic@gmail.com>
2018-02-06 18:51:15 +0600

    code updated

--- fitness_care/urls.py
+++ fitness_care/urls.py
@@ -2,4 +2,6 @@ from django.urls import path,re_path
 from fitness_care import views
 urlpatterns = [
     path("index/",views.index,name="index"),
+    path("test/",views.test,name="test"),
+
 ]
--- fitness_care/views.py
+++ fitness_care/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    text = "Fitness care gym"
-    return HttpResponse(text)
+    # text = "Fitness care gym"
+    # return HttpResponse(text)
+    context_dict = {}
+    context_dict["name"] = "sharupa"
+    return render(request,"fitness_care/index.html",context = context_dict)
+
+
+def test(request):
+     return render(request,"fitness_care/test.html")
+
+def pic(request):
+    return render(request,"fitness_care/pic.css")
